13e78c2689c337bade25864e36ff66bdd8c0a650
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2022-05-03 23:13:35 +0100

    Updated network helper IP

--- software/network_helper/setup.sh
+++ software/network_helper/setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-ifconfig enp0s25 10.11.12.2/24
-route add -host 1.1.1.1 gw 10.11.12.1
+#ifconfig enp0s25 10.11.12.2/24
+#route add -host 1.1.1.1 gw 10.11.12.1
 sysctl -w net.ipv4.ip_forward=1
 iptables -A FORWARD -i enp0s25 -j ACCEPT
 iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
